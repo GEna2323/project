24f4acaaa840f16905e2734a479e0b859297a67d
GEna2323 <bilykgv@gmail.com>
2023-07-13 18:22:56 +0300

    hello

--- index.php
+++ index.php
@@ -5,15 +5,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Project</title>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="reset.css">
+    <link href="https://fonts.googleapis.com/css?family=Chivo:regular,500,600,700" rel="stylesheet"/>
+    <link rel="stylesheet" href="">
 </head>
-
 <body>
-    <div class="wraper">
+    <div class="wrapper">
         <header class="header">
             <div class="header__container _container">
-                
+                <a href="" class="header__logo">
+                    V1kt0R
+                </a>
+                <nav class="header__menu menu">
+                    <ul class="menu__list">
+                        <li class="menu__item">
+                            <a href="" class="menu__href"></a>
+                        </li>
+                        <li class="menu__item">
+                            <a href="" class="menu__href"></a>
+                        </li>
+                        <li class="menu__item">
+                            <a href="" class="menu__href"></a>
+                        </li>
+                        <li class="menu__item">
+                            <a href="" class="menu__href"></a>
+                        </li>
+                    </ul>
+                </nav>
             </div>
         </header>
         <main class="main">
