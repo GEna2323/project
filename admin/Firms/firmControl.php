ba5a34439b0284b5a9a8c277b7d6441c40dae62d
Viktor <Viktor0732@ukr.net>
2023-07-15 16:16:23 +0300

    14:28

---
+++ admin/Firms/firmControl.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Firm Control</title>
+</head>
+<body>
+<?php
+    require_once("../param.php");
+
+    $query = "select id, name from Firms";
+
+    $result = mysqli_query($dbc, $query) or die("Query Error");
+
+    echo "<table border='2'><tr><th>№</th><th>Firma</th><th>Delete</th></tr>";
+
+    $num = 1;
+    while($row = mysqli_fetch_array($result)){
+
+        echo "<tr><td>$num</td><td>{$row['name']}</td><td><a href='dellFirma.php?id={$row['id']}&firma={$row['name']}'>Click</a></td></tr>";
+        $num++;
+    }
+
+    mysqli_close($dbc);
+
+?>
+</body>
+</html>
