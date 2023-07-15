1f29f16b9ce91364a7f8d6a3b14b33af98888137
Viktor <Viktor0732@ukr.net>
2023-07-15 15:17:45 +0300

    14:28

--- admin/param.php
+++ admin/param.php
@@ -1,6 +1,6 @@
 <?php
 
-$dbc = mysqli_connect('localhost','root','','GVShop','3306') or die("Server Error");
+$dbc = mysqli_connect('fdb1028.awardspace.net','4345972_gvshop','v%_xKRvG9;0VEtku','4345972_gvshop','3306') or die("Server Error");
 
 ?>
 
