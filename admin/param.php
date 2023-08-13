eab68ae3aa35e9c2a8d82dc713b2827f5cf2045f
Viktor <Viktor0732@ukr.net>
2023-08-13 19:47:35 +0300

    Глобальне удаление

--- admin/param.php
+++ admin/param.php
@@ -2,7 +2,7 @@
 
 //$dbc = mysqli_connect('fdb1028.awardspace.net','4345972_gvshop','v%_xKRvG9;0VEtku','4345972_gvshop','3306') or die("Server Error");
 
-$dbc = mysqlI_connect('localhost','root','','GVShop') or die("Server Error");
+$dbc = mysqli_connect('localhost','root','','GVShop') or die("Server Error");
 
 ?>
 
