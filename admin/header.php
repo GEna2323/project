e7c7e36fed22dcf7c36ed20d4c8753f5fee6d0d9
Viktor <Viktor0732@ukr.net>
2023-08-13 19:58:28 +0300

    Глобальне удаление

---
+++ admin/header.php
@@ -0,0 +1,12 @@
+<?php
+
+    define('SMARTY_LIB', '../libs/');
+
+    require_once(SMARTY_LIB.'Smarty.class.php');
+
+    $smarty_main = new Smarty();
+
+    require_once('param.php');
+
+
+?>
--- admin/param.php
+++ admin/param.php
@@ -4,5 +4,5 @@
 
 $dbc = mysqli_connect('localhost','root','','GVShop') or die("Server Error");
 
-?>
 
+?>
